1687d29bbffb871adc2191ce65303213110af147
gizmore <gizmore@wechall.net>
2017-10-07 23:37:43 +0200

    6.05_fixpack

--- Module_Dog.php
+++ Module_Dog.php
@@ -1,10 +1,15 @@
 <?php
 namespace GDO\Dog;
 use GDO\Core\GDO_Module;
-use GDO\Dog\Connector;
 
+/**
+ * Port of Lamb3... but not even started yet.
+ * @author gizmore
+ */
 final class Module_Dog extends GDO_Module
 {
+	public function defaultEnabled() { return false; }
+	
     public function onInit()
     {
 //         Connector::init();
