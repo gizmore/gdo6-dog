a7cf002f0808e852c9771dedee3c16d2dd34630b
gizmore <gizmore@wechall.net>
2020-10-11 05:55:44 +0200

    6.10-r9201

---
+++ DOG_Timer.php
@@ -0,0 +1,31 @@
+<?php
+namespace GDO\Dog;
+
+final class DOG_Timer
+{
+    public static $TIMERS = [];
+    
+    public static function addTimer(DOG_Timer $timer)
+    {
+        self::$TIMERS[] = $timer;
+    }
+    
+    public $repeat = false;
+    public function repeat($repeat=true)
+    {
+        $this->repeat = $repeat;
+        return $this;
+    }
+    
+    public 
+    public function in($in)
+    {
+        
+    }
+    
+    public function __construct()
+    {
+        
+    }
+    
+}
