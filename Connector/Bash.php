171c9c09499c393f3e2248fd50a12e5b85b9ba11
gizmore <gizmore@wechall.net>
2021-02-28 04:06:45 +0100

    htmlName_removed

--- Connector/Bash.php
+++ Connector/Bash.php
@@ -22,37 +22,38 @@ class Bash extends DOG_Connector
 {
     private $bashServer;
     
-	public function init()
-	{
-		if (!($this->bashServer = $this->getBashServer()))
-		{
-			$this->bashServer = DOG_Server::table()->blank(array(
-				'serv_connector' => $this->gdoShortName(),
-			))->insert();
-		}
-	}
-	
-	/**
-	 * @return DOG_Server
-	 */
-	public function getBashServer()
-	{
-	    if ($this->bashServer)
-	    {
-	        return $this->bashServer;
-	    }
-		$query = DOG_Server::table()->select('*');
-		$query->where("serv_connector='{$this->gdoShortName()}'")->first();
-		return $query->exec()->fetchObject();
-	}
-	
-	public function getBashUser()
-	{
-	    $user = DOG_User::getOrCreateUser($this->getBashServer(), get_current_user());
-	    GDO_User::$CURRENT = $user->getGDOUser();
-	    return $user;
-	}
-	
+    public function init()
+    {
+        if (!($this->bashServer = $this->getBashServer()))
+        {
+            $this->bashServer = DOG_Server::table()->blank(array(
+                'serv_connector' => $this->gdoShortName(),
+            ))->insert();
+        }
+    }
+    
+    /**
+     * @return DOG_Server
+     */
+    public function getBashServer()
+    {
+        if ($this->bashServer)
+        {
+            return $this->bashServer;
+        }
+        $query = DOG_Server::table()->select('*')->
+          where("serv_connector='{$this->gdoShortName()}'")->
+          first();
+        return $query->exec()->fetchObject();
+    }
+    
+    public function getBashUser()
+    {
+        $user = DOG_User::getOrCreateUser($this->getBashServer(), get_current_user());
+        GDO_User::$CURRENT = $user->getGDOUser();
+        return $user;
+    }
+    
     public function connect()
     {
         $this->connected = true;
@@ -66,30 +67,30 @@ class Bash extends DOG_Connector
     }
     
     
-	public function readMessage()
-	{
-	    return false;
-	}
-	
-	public function dog_cmdline(...$argv)
-	{
-		$text = implode(' ', $argv);
-		$msg = DOG_Message::make()->
-			server($this->getBashServer())->
-			user($this->getBashUser())->
-			text($text);
-		Dog::instance()->event('dog_message', $msg);
-	}
+    public function readMessage()
+    {
+        return false;
+    }
+    
+    public function dog_cmdline(...$argv)
+    {
+        $text = implode(' ', $argv);
+        $msg = DOG_Message::make()->
+            server($this->getBashServer())->
+            user($this->getBashUser())->
+            text($text);
+        Dog::instance()->event('dog_message', $msg);
+    }
 
-	public function sendToUser(DOG_User $user, $text)
-	{
-	    echo "$text\n";
-	}
-	
-	public function sendToRoom(DOG_Room $room, $text)
-	{
-	    echo "$text\n";
-	}
+    public function sendToUser(DOG_User $user, $text)
+    {
+        echo "$text\n";
+    }
+    
+    public function sendToRoom(DOG_Room $room, $text)
+    {
+        echo "$text\n";
+    }
    
     public function sendNoticeToUser(DOG_User $user, $text)
     {
